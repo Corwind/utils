900d458674b44fd5f5a1d42c0ffcb958d57879cf
Guillaume Doré <guillaume@dore-net.fr>
2021-09-15 18:35:32 +0200

    initial commit with dbutils

---
+++ dbutils/dbutils.go
@@ -0,0 +1,56 @@
+package dbutils
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+	"github.com/apple/foundationdb/bindings/go/src/fdb/tuple"
+)
+
+func DbSaveEntity(db fdb.Database, t tuple.Tuple, buffer *bytes.Buffer) (interface{}, error) {
+	return db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+		tr.Set(t, buffer.Bytes())
+		return tr.Get(t).Get()
+	})
+}
+
+func DbFetchEntity(db fdb.Database, layer string, id string) (interface{}, error) {
+	return db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+		ret, err := tr.Get(tuple.Tuple{layer, id}).Get()
+		if err != nil {
+			return nil, err
+		}
+		if ret == nil {
+			return nil, fmt.Errorf("%s not found", layer)
+		}
+		return ret, err
+	})
+}
+
+func DbFetchRange(db fdb.Database, range_ tuple.Tuple) ([]interface{}, error) {
+	values := make([]interface{}, 0)
+
+	_, err := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+		rangePrefix, err := fdb.PrefixRange(range_.Pack())
+		if err != nil {
+			return nil, err
+		}
+		rangeIterator, err := tr.GetRange(rangePrefix, fdb.RangeOptions{}).Iterator(), nil
+		if err != nil {
+			return nil, err
+		}
+
+		for rangeIterator.Advance() {
+			kv := rangeIterator.MustGet()
+			values = append(values, kv.Value)
+		}
+
+		return nil, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return values, nil
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Corwind/utils
+
+go 1.17
+
+require github.com/apple/foundationdb/bindings/go v0.0.0-20210915153027-935d7b3cdcd3
+
+require golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
